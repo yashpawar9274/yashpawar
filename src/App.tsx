8b35dd55ba69696dc6a0fbf46a7367095b03a64c
yashpawar9274 <yash.pawar@theemcoe.org>
2025-10-18 20:17:58 +0530

    Merge remote changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
@@ -14,6 +15,7 @@ import AuthPage from "./components/auth/AuthPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import PWAInstaller from "./components/PWAInstaller";
 import { useAppIcon } from "./hooks/useAppIcon";
+
 
 const App: React.FC = () => {
   // Initialize PWA icon updates
